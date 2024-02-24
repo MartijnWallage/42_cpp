591ffe56ad360034a9bf1b9747f20de678951f30
Martijn Wallage <67522397+MartijnWallage@users.noreply.github.com>
2024-02-24 15:46:04 +0000

    Better eof handling

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mwallage <mwallage@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 14:17:33 by mwallage          #+#    #+#             */
-/*   Updated: 2024/02/22 14:17:37 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/02/24 15:44:23 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@ int	main() {
 	PhoneBook	phoneBook;
 	std::string	command;
 
-	while (true) {
+	while (command != "EXIT") {
 		std::cout << "Enter command (ADD, SEARCH, EXIT): ";
 		std::getline(std::cin, command);
 
-		if (command == "EXIT") {
-			break ;
-		} else if (command == "ADD") {
+		if (command == "ADD") {
 			phoneBook.addContact();
 		} else if (command == "SEARCH") {
 			phoneBook.searchContact();
+		} else if (std::cin.eof()) {
+			std::cout << std::endl;
+			return 0;
 		} else {
 			std::cout << "Invalid command." << std::endl;
 		}
