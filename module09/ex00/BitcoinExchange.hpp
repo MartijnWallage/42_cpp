0aa55050131fdb510fe1a14236b117e523f2aa87
Martijn Wallage <mwallage@student.42berlin.de>
2024-04-22 15:32:06 +0200

    reading

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,16 @@
 /*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:37:51 by mwallage          #+#    #+#             */
-/*   Updated: 2024/04/22 13:45:39 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/04/22 15:21:55 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
 
 // Let's use maps for this
 // Associate dates with values
--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:37:54 by mwallage          #+#    #+#             */
-/*   Updated: 2024/04/22 13:48:29 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/04/22 15:31:59 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,56 @@ int main( int argc, char *argv[]) {
 		return 1;
 	}
 
-	// open(argv[1]);
-	// if (fail) {exit};
-	// parse input as map;
-	// parse data.csv as map
+	// parse input as map
+	std::ifstream inputFile(argv[1]);
+	if (!inputFile.is_open())
+	{
+		std::cerr << "Failed to open " << argv[1] << "." << std::endl;
+		return 2;
+	}
+	std::map<std::string, float> inputMap;
+	std::string line;
+	while (std::getline(inputFile, line))
+	{
+		std::istringstream iss(line);
+		std::string date;
+		float value;
+		char dash;
+		if (std::getline(iss, date, '|') && iss >> value >> dash) {
+			inputMap[date] = value;
+		} else {
+			std::cerr << "Error parsing line in input: " << line << std::endl;
+		}
+	}
+	inputFile.close();
+
+	std::ifstream file("data.csv");
+	if (!file.is_open())
+	{
+		std::cerr << "Failed to open data.csv." << std::endl;
+		return 2;		
+	}
+
+	std::map<std::string, float> dataMap;
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::istringstream iss(line);
+		std::string date;
+		float value;
+		char comma;
+		if (std::getline(iss, date, ',') && iss >> value >> comma) {
+			dataMap[date] = value;
+		} else {
+			std::cerr << "Error parsing line in database: " << line << std::endl;
+		}
+	}
+	file.close();
+
 	// loop through input map. For each line:
-		// if (!isValidInput()) {exit}
 		// find nearest data in data.csv, return exchange value
 		// multiply exchange value with given amount
 		// output result
+
 	return 0;
 }
