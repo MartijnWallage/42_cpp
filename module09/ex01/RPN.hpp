18bfb650a0b0c7892e8fac840db41a87727ed0ca
Martijn Wallage <mwallage@student.42berlin.de>
2024-04-22 19:55:19 +0200

    Getting far

--- module09/ex01/RPN.hpp
+++ module09/ex01/RPN.hpp
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mwallage <mwallage@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:52:30 by mwallage          #+#    #+#             */
-/*   Updated: 2024/04/22 14:05:46 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/04/22 19:38:46 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
 
 // Let's use a stack for this
--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -3,14 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mwallage <mwallage@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:52:48 by mwallage          #+#    #+#             */
-/*   Updated: 2024/04/22 14:06:18 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/04/22 19:54:11 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+int parseNbr( std::stringstream & ss, std::stack<int> & stack)
+{
+	int nbr;
+
+	ss >> nbr;
+	if (ss.fail()) {
+		std::cerr << "Error" << std::endl;
+		return 0;
+	}
+	stack.push(nbr);
+	return 1;
+}
+
+int parseOp( std::stringstream & ss, std::stack<int> & stack)
+{
+	char op;
+
+	ss >> op;
+	if (ss.fail()) {
+		std::cerr << "Error" << std::endl;
+		return 0;
+	}
+	return 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,7 +46,30 @@ int main(int argc, char *argv[])
 	}
 
 	// check valid input
-	// parse input as stack
-	// calculate result
-	// output result
+
+	std::stack<int> stack;
+	std::stringstream ss(argv[1]);
+	int		nbr;
+	char	op;
+
+	if (!parseNbr(ss, stack))
+		return 1;
+
+	bool expectingNbr = true;
+	while (!ss.eof()) {
+		if (expectingNbr) {
+			if (!parseNbr(ss, stack))
+				return 1;
+		} else {
+			if (!parseOp(ss, stack))
+				return 1;
+		}
+		expectingNbr != expectingNbr;
+	}
+	if (stack.size() != 1) {
+		std::cerr << "Error" << std::endl;
+		return 1;
+	}
+	std::cout << stack.top() << std::endl;
+	return 0;
 }
