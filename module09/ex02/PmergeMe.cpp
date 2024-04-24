a9be503b219de8ad2c7d074103b93eb5b86841fd
Martijn Wallage <mwallage@student.42berlin.de>
2024-04-24 15:55:32 +0200

    Beginning

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -6,8 +6,40 @@
 /*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 14:08:03 by mwallage          #+#    #+#             */
-/*   Updated: 2024/04/23 15:00:53 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/04/24 15:22:15 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+PMergeMe::PMergeMe(std::list<int> const &unsorted) : _list(unsorted)
+{
+}
+
+PMergeMe::PMergeMe(std::vector<int> const &unsorted) : _vec(unsorted)
+{
+}
+
+PMergeMe::PMergeMe(PMergeMe const &src) : _list(src._list), _vec(src._vec)
+{
+}
+
+PMergeMe PMergeMe::operator=(PMergeMe const &src)
+{
+	if (this != &src)
+	{
+		_list = src._list;
+		_vec = src._vec;
+	}
+	return *this;
+}
+
+PMergeMe::~PMergeMe() {}
+
+std::list<int> PMergeMe::sort(std::list<int>)
+{
+}
+
+std::vector<int> PMergeMe::sort(std::vector<int>)
+{
+}
--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -6,11 +6,30 @@
 /*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 14:08:07 by mwallage          #+#    #+#             */
-/*   Updated: 2024/04/22 15:11:20 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/04/24 15:43:19 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <vector>
+#include <list>
 
-// maybe use vector and list
+class PMergeMe
+{
+public:
+	PMergeMe(std::list<int> const &);
+	PMergeMe(std::vector<int> const &);
+	~PMergeMe();
+
+	std::list<int> sort(std::list<int>);
+	std::vector<int> sort(std::vector<int>);
+
+private:
+	PMergeMe(PMergeMe const &);
+	PMergeMe operator=(PMergeMe const &);
+
+	int _getNextJacobsthal(int);
+	std::list<int> _list;
+	std::vector<int> _vec;
+};
